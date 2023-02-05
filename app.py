6a44859484aaf58d4eb4ac547480e3ab14ba6cd7
DanBrown47 <danwand47@gmail.com>
2023-02-06 04:55:15 +0530

    Change : main.py -> app.py | Best practices

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from src.res import Fetcher
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return "We're Building an API for IPC"
+
+
+@app.route(rule='/ipc', methods=['GET'])
+def ipc_Fetch():
+    f = Fetcher()
+    return f.ret_all()
+
+
+
+@app.route('/ipc/<section>', methods=['GET','POST'])
+def section_Fetch(section):
+    f = Fetcher()
+    return f.ret_id(section)
+
+if __name__ == "__main__":
+    app.run(debug=True)
