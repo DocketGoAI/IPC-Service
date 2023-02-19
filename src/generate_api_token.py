ce0c3f2cb5acd6bb4f3903e7c586fc01fc5a4823
mhdali <alikaithayil2@gmail.com>
2023-02-19 14:09:15 +0530

    changed sqlite3 to sqlalchemy

--- src/generate_api_token.py
+++ src/generate_api_token.py
@@ -1,41 +1,42 @@
 import random
 import string
-import sqlite3
+from sqlalchemy import create_engine, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# Connect to the database (or create it if it doesn't exist)
-conn = sqlite3.connect('Tokens.db', check_same_thread=False)
-cursor = conn.cursor()
+# db configuration
+engine = create_engine('sqlite:///Tokens.db', echo=True)
+Session = sessionmaker(bind=engine)
 
-# Create the table to store the tokens (if it doesn't already exist)
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Tokens (
-    token_text text
-)
-''')
-conn.commit()
+# Create a base class for the ORM model
+Base = declarative_base()
 
 
-class ApiToken:
-    
-    def generate_token():
-        # Generate random alpha-numeric string with length 50
+class ApiToken(Base):
+    __tablename__ = 'Tokens'
+    token_text = Column(String, primary_key=True)
+
+    def generate_token(cls):
+        '''
+        generate random alpha-numeric string with length 50 and check whether it already exist or not
+        '''
         token = ''.join(random.choices(string.ascii_letters+string.digits, k=40))
-        result = cursor.execute("SELECT * FROM Tokens WHERE token_text=?", (token,)).fetchone()
-        
-        # Check whether the token already exist or not
-        while result:
-            token = ''.join(random.choices(string.ascii_letters+string.digits, k=50))
-            result = cursor.execute("SELECT * FROM Tokens WHERE token_text=?", (token,)).fetchone()
-        
-        cursor.execute("INSERT INTO Tokens (token_text) VALUES (?)", (token,))
-        conn.commit()
+        with Session() as session:
+            result = session.query(cls).filter_by(token_text=token).first()
+            while result:
+                token = ''.join(random.choices(string.ascii_letters+string.digits, k=50))
+                result = session.query(cls).filter_by(token_text=token).first()
+            new_token = cls(token_text=token)
+            session.add(new_token)
+            session.commit()
+            return token
 
-        return token
-    
-    
-    def is_valid_token(token_to_check):
-        cursor.execute("SELECT * FROM Tokens WHERE token_text=?", (token_to_check,))
-        result = cursor.fetchone()
+    def is_valid_token(cls, token_to_check):
+        with Session() as session:
+            result = session.query(cls).filter_by(token_text=token_to_check).first()
+        if result:
+            return True
+        return False
 
-        return True if result else False    
-
+# create the table (if it doesn't already exist)
+Base.metadata.create_all(engine)
